63319c2c08847659ac0b7725b2fd0d20fd7cb7e9
Bill Durr <billyburly@gmail.com>
2014-03-24 21:17:55 -0400

    proper gateway implementation. add debug define

--- gateway/gateway.ino
+++ gateway/gateway.ino
@@ -6,27 +6,36 @@
 #include <rf12-common.h>
 #include <tmp102.h>
 
-int humidPin = A0;
-
-int itemp;
-int ihumid;
-
 byte ip[] = { 10, 47, 0, 80 };
 byte gateway[] = { 10, 47, 0, 1 };
 byte subnet[] = { 255, 255, 255, 0 };
 
-bool farenheight = true;
-
 byte Ethernet::buffer[500];
 BufferFiller bfill;
 #define NODEID 1
-#define SEN1_NODE 2
+#define MAXNODES 2 //max nodes for network is 30. ran into stability issues. ethernet buffer too small?
+//#define DEBUG
 
 DataStructure payload;
-int ledPin = 6;
+#define LED_PIN 6
+
+char okHeader[] PROGMEM =
+  "HTTP/1.0 200 OK\r\n"
+  "Content-Type: text/plain\r\n"
+  "Pragma: no-cache\r\n\r\n"
+;
+
+struct Node {
+  int temp;
+  int humid;
+  unsigned long updated_at;
+};
+struct Node Nodes[MAXNODES];
 
 void setup () {
+#ifdef DEBUG
   Serial.begin(9600);
+#endif
   Wire.begin();
   
   byte macaddr[6];
@@ -36,11 +45,19 @@ void setup () {
   ether.begin(sizeof Ethernet::buffer, macaddr);
   ether.staticSetup(ip, gateway);
   
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
   
   rf12_initialize(NODEID,RF12_433MHZ,NETWORK);
+  
+  for(int i = 0; i < MAXNODES; i++) {
+    Nodes[i].temp = -9999;
+    Nodes[i].humid = -9999;
+    Nodes[i].updated_at = 0;
+  }
+#ifdef DEBUG
   Serial.println("init done");
+#endif
 }
 
 void loop () {
@@ -48,25 +65,43 @@ void loop () {
   word pos = ether.packetLoop(len);
 
   if (rf12_recvDone() && rf12_crc == 0) {
-    if ((rf12_hdr & 0x1F) == SEN1_NODE) {
-      payload = *(DataStructure*) rf12_data;
-    }
+    byte recvId = (rf12_hdr & 0x1F) -1;
+    payload = *(DataStructure*) rf12_data;
+    
+    Nodes[recvId].temp = payload.temp;
+    Nodes[recvId].humid = payload.humid;
+    Nodes[recvId].updated_at = millis();
+#ifdef DEBUG
+    Serial.print("rcv:");
+    Serial.print(recvId);
+    Serial.print(":");
+    Serial.print(payload.temp);
+    Serial.print(",");
+    Serial.print(payload.humid);
+    Serial.print(",");
+    Serial.println(Nodes[recvId].updated_at);
+#endif
   }
   
   if(pos) {
-    itemp = readTemp();
+#ifdef DEBUG
+    Serial.println("reading local sensors");
+#endif
+    Nodes[NODEID-1].temp = readTemp();
     //read humidity from HIH-4030
-    ihumid = analogRead(humidPin);
+    Nodes[NODEID-1].humid = analogRead(A0);
+    Nodes[NODEID-1].updated_at = millis();
     
+#ifdef DEBUG
+    Serial.println("preparing file to send");
+#endif
     bfill = ether.tcpOffset();
-    bfill.emit_p(PSTR(
-      "HTTP/1.0 200 OK\r\n"
-      "Content-Type: text/plain\r\n"
-      "Pragma: no-cache\r\n"
-      "\r\n"
-      "0:$D,$D\r\n"
-      "2:$D,$D"),
-      itemp, ihumid, payload.temp, payload.humid);
+    bfill.emit_p(PSTR("$F"), okHeader);
+    for(int i = 0; i < MAXNODES; i++)
+      bfill.emit_p(PSTR("$D:$D,$D,$D\r\n"), i, Nodes[i].temp, Nodes[i].humid, Nodes[i].updated_at);
     ether.httpServerReply(bfill.position());
+#ifdef DEBUG
+    Serial.println("sending data over http");
+#endif
   }  
 }
